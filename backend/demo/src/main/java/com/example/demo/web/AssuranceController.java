123df4fbd03bc3f747c70a5d37703ab5d9b9524c
AITOULAHYAN FATIMA <aitoulahyanfatima310@gmail.com>
2026-05-11 11:47:00 +0100

    add frontend

---
+++ backend/demo/src/main/java/com/example/demo/web/AssuranceController.java
@@ -0,0 +1,11 @@
+package com.example.demo.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@AllArgsConstructor
+public class AssuranceRestController {
+}
